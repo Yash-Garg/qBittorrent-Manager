508ba6a09f7172f12689c26eee789151f90fb643
Yash-Garg <yash.garg@outlook.in>
2022-07-26 20:54:22 +0530

    chore: add a task for copying git hooks on windows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @file:Suppress("DSL_SCOPE_VIOLATION")
+
+import com.android.build.gradle.internal.tasks.factory.dependsOn
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -8,4 +11,14 @@ plugins {
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.navigation.safeargs) apply false
+}
+
+apply("buildScripts/githooks.gradle.kts")
+
+val clean by tasks.registering(Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+afterEvaluate {
+    clean.dependsOn("copyGitHooks")
 }
---
+++ hooks/pre-commit.sh
@@ -0,0 +1,5 @@
+#!C:/Program Files/Git/bin/sh.exe
+
+echo 'Running spotlessApply'
+
+./gradlew spotlessApply
