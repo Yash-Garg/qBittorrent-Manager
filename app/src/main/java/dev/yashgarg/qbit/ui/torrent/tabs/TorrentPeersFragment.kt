95a735d127877c37bcb6d1b21e09f71c74146636
Yash-Garg <yash.garg@outlook.in>
2022-08-04 12:27:24 +0530

    chore (ui): some list view changes on peers fragment

--- app/src/main/java/dev/yashgarg/qbit/ui/torrent/tabs/TorrentPeersFragment.kt
+++ app/src/main/java/dev/yashgarg/qbit/ui/torrent/tabs/TorrentPeersFragment.kt
@@ -2,6 +2,7 @@ package dev.yashgarg.qbit.ui.torrent.tabs
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.LinearProgressIndicator
@@ -9,7 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.composethemeadapter3.Mdc3Theme
@@ -42,9 +47,18 @@ fun PeersListView(state: TorrentDetailsState) {
     } else if (state.peers.peers.isEmpty()) {
         Center { Text("No peers connected") }
     } else {
+        val peers = requireNotNull(state.peers).peers.values.toList()
         LazyColumn {
-            itemsIndexed(requireNotNull(state.peers).peers.values.toList()) { _, peer ->
-                Text(peer.client)
+            itemsIndexed(peers) { _, peer ->
+                Text(
+                    "${peer.country} - ${peer.ip}",
+                    modifier = Modifier.padding(8.dp),
+                    style =
+                        TextStyle(
+                            fontSize = 15.sp,
+                            color = colorResource(R.color.white),
+                        )
+                )
             }
         }
     }
