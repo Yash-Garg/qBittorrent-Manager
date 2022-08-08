be24f5ec5b7bf98768f83867ec526c9907972714
Yash-Garg <yash.garg@outlook.in>
2022-08-08 10:15:34 +0530

    refactor: some misc ui changes to Bonsai tree view

--- ui-compose/src/main/kotlin/dev/yashgarg/qbit/ui/compose/TorrentContentTreeView.kt
+++ ui-compose/src/main/kotlin/dev/yashgarg/qbit/ui/compose/TorrentContentTreeView.kt
@@ -1,15 +1,22 @@
 package dev.yashgarg.qbit.ui.compose
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FileCopy
+import androidx.compose.material.icons.rounded.Folder
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.bonsai.core.Bonsai
 import cafe.adriel.bonsai.core.BonsaiStyle
 import cafe.adriel.bonsai.core.node.Branch
+import cafe.adriel.bonsai.core.node.BranchNode
 import cafe.adriel.bonsai.core.node.Leaf
 import cafe.adriel.bonsai.core.tree.Tree
 import cafe.adriel.bonsai.core.tree.TreeScope
@@ -19,20 +26,32 @@ import dev.yashgarg.qbit.ui.compose.theme.SpaceGrotesk
 @Composable
 fun TorrentContentTreeView(nodes: List<ContentTreeItem>) {
     val tree = Tree<ContentTreeItem> { ContentTree(nodes) }
+    tree.expandRoot()
 
     Bonsai(
         tree,
-        style =
-            BonsaiStyle(
-                nodeNameTextStyle =
-                    TextStyle(
-                        color = Color.White,
-                        fontSize = 14.sp,
-                        fontFamily = SpaceGrotesk,
-                    ),
-                toggleIconColorFilter = ColorFilter.tint(Color.White)
+        style = torrentContentStyle(),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        onLongClick = null,
+    )
+}
+
+private fun torrentContentStyle(): BonsaiStyle<ContentTreeItem> {
+    return BonsaiStyle(
+        nodeNameTextStyle =
+            TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontFamily = SpaceGrotesk,
             ),
-        modifier = Modifier.fillMaxSize()
+        toggleIconColorFilter = ColorFilter.tint(Color.LightGray),
+        nodeCollapsedIcon = { node ->
+            rememberVectorPainter(
+                if (node is BranchNode) Icons.Rounded.Folder else Icons.Default.FileCopy
+            )
+        },
+        nodeCollapsedIconColorFilter = ColorFilter.tint(Color.LightGray),
+        nodeNameStartPadding = 8.dp
     )
 }
 
@@ -43,6 +62,7 @@ private fun TreeScope.ContentTree(nodes: List<ContentTreeItem>) {
 
 @Composable
 private fun TreeScope.ContentNode(node: ContentTreeItem) {
+    // If [node.item] is null, therefore it is a directory
     if (node.item == null) {
         Branch(node.name) { node.children?.reversed()?.let { ContentTree(it) } }
     } else {
