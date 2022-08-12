73f0d6185f62e2ffb4ac471b1e343f4b8fb1f80a
Yash-Garg <yash.garg@outlook.in>
2022-08-12 16:02:41 +0530

    refactor: remove xml layout for Files fragment and use ComposeView

--- app/src/main/kotlin/dev/yashgarg/qbit/ui/torrent/tabs/TorrentFilesFragment.kt
+++ app/src/main/kotlin/dev/yashgarg/qbit/ui/torrent/tabs/TorrentFilesFragment.kt
@@ -1,32 +1,40 @@
 package dev.yashgarg.qbit.ui.torrent.tabs
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.composethemeadapter3.Mdc3Theme
-import dev.yashgarg.qbit.R
-import dev.yashgarg.qbit.databinding.TorrentFilesFragmentBinding
 import dev.yashgarg.qbit.ui.compose.Center
 import dev.yashgarg.qbit.ui.compose.TorrentContentTreeView
 import dev.yashgarg.qbit.ui.torrent.TorrentDetailsState
 import dev.yashgarg.qbit.ui.torrent.TorrentDetailsViewModel
-import dev.yashgarg.qbit.utils.viewBinding
 
-class TorrentFilesFragment : Fragment(R.layout.torrent_files_fragment) {
-    private val binding by viewBinding(TorrentFilesFragmentBinding::bind)
+class TorrentFilesFragment : Fragment() {
     private val viewModel by
         viewModels<TorrentDetailsViewModel>(ownerProducer = { requireParentFragment() })
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.filesComposeView.setContent {
-            val state by viewModel.uiState.collectAsState()
-            Mdc3Theme(setTextColors = true, setDefaultFontFamily = true) { FilesListView(state) }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val composeView = ComposeView(requireContext())
+        composeView.apply {
+            setContent {
+                val state by viewModel.uiState.collectAsState()
+                Mdc3Theme(setTextColors = true, setDefaultFontFamily = true) {
+                    FilesListView(state)
+                }
+            }
         }
+
+        return composeView
     }
 }
 
