4b9deb7adaac1e8c27bbbce938824dc47099d7ed
Yash-Garg <yash.garg@outlook.in>
2022-09-07 18:27:16 +0530

    chore: cleanup unused xml drawables

--- app/src/main/kotlin/dev/yashgarg/qbit/QbitApplication.kt
+++ app/src/main/kotlin/dev/yashgarg/qbit/QbitApplication.kt
@@ -3,9 +3,4 @@ package dev.yashgarg.qbit
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
-class QbitApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+@HiltAndroidApp class QbitApplication : Application()
