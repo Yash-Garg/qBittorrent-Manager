3cde9c89f39b5b64c0b34166220539ebb489780c
Yash-Garg <yash.garg@outlook.in>
2022-09-11 16:59:02 +0530

    feat(ui): add compose impl for ListTile and use in PeersFragment

--- app/src/main/kotlin/dev/yashgarg/qbit/ui/torrent/tabs/TorrentPeersFragment.kt
+++ app/src/main/kotlin/dev/yashgarg/qbit/ui/torrent/tabs/TorrentPeersFragment.kt
@@ -2,16 +2,19 @@ package dev.yashgarg.qbit.ui.torrent.tabs
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -19,6 +22,7 @@ import com.google.android.material.composethemeadapter3.Mdc3Theme
 import dev.yashgarg.qbit.R
 import dev.yashgarg.qbit.databinding.TorrentPeersFragmentBinding
 import dev.yashgarg.qbit.ui.compose.Center
+import dev.yashgarg.qbit.ui.compose.ListTile
 import dev.yashgarg.qbit.ui.torrent.TorrentDetailsState
 import dev.yashgarg.qbit.ui.torrent.TorrentDetailsViewModel
 import dev.yashgarg.qbit.utils.viewBinding
@@ -33,7 +37,9 @@ class TorrentPeersFragment : Fragment(R.layout.torrent_peers_fragment) {
 
         binding.peersComposeView.setContent {
             val state by viewModel.uiState.collectAsState()
-            Mdc3Theme(setTextColors = true, setDefaultFontFamily = true) { PeersListView(state) }
+            Mdc3Theme(setTextColors = true, readTypography = true, setDefaultFontFamily = true) {
+                PeersListView(state)
+            }
         }
     }
 }
@@ -48,9 +54,18 @@ fun PeersListView(state: TorrentDetailsState) {
         val peers = requireNotNull(state.peers).peers.values.toList()
         LazyColumn {
             itemsIndexed(peers) { _, peer ->
-                Text(
-                    "${peer.country} - ${peer.ip}",
-                    modifier = Modifier.padding(8.dp),
+                ListTile(
+                    modifier = Modifier.fillMaxWidth().padding(18.dp),
+                    title = "${peer.ip}:${peer.port}",
+                    subtitle = "Connection: ${peer.connection}",
+                    suffix = {
+                        Text(
+                            peer.countryCode.uppercase(),
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    },
                 )
             }
         }
