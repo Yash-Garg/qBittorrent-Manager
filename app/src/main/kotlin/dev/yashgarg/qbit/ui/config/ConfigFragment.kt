5e12bd55a148b0cc9b86553c7f9bb87c6247e6fb
Yash-Garg <yash.garg@outlook.in>
2022-10-06 14:17:35 +0530

    chore: add basic widget impl with Glance

--- app/src/main/kotlin/dev/yashgarg/qbit/ui/config/ConfigFragment.kt
+++ app/src/main/kotlin/dev/yashgarg/qbit/ui/config/ConfigFragment.kt
@@ -133,10 +133,12 @@ class ConfigFragment : Fragment(R.layout.config_fragment) {
                 checkSnackbar.show()
 
                 viewLifecycleOwner.lifecycleScope.launch {
+                    val port = binding.serverPortTil.editText?.text.toString()
                     val connectionResponse =
                         viewModel.testConfig(
                             "${binding.typeDropdown.editText?.text.toString().lowercase()}://" +
-                                "${binding.serverHostTil.editText?.text}:${binding.serverPortTil.editText?.text}",
+                                "${binding.serverHostTil.editText?.text}" +
+                                if (port != "443") ":$port" else "",
                             binding.serverUsernameTil.editText?.text.toString(),
                             binding.serverPasswordTil.editText?.text.toString(),
                         )
---
+++ ui-compose/src/main/kotlin/dev/yashgarg/qbit/ui/compose/widgets/TorrentListWidget.kt
@@ -0,0 +1,64 @@
+package dev.yashgarg.qbit.ui.compose.widgets
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.toColorInt
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.ActionParameters
+import androidx.glance.action.clickable
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.text.FontWeight
+import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+
+class TorrentListWidget : GlanceAppWidget() {
+
+    @Composable
+    override fun Content() {
+        Column(
+            modifier =
+                GlanceModifier.fillMaxSize()
+                    .background(Color("#353A4E".toColorInt()))
+                    .clickable(onClick = actionRunCallback<TitleClickAction>()),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Test Widget with Glance (Compose)",
+                style =
+                    TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = ColorProvider(Color.White)
+                    ),
+            )
+        }
+    }
+}
+
+class TorrentListWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget
+        get() = TorrentListWidget()
+}
+
+class TitleClickAction : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        Log.d(TorrentListWidget::class.simpleName, "widget onClick() callback")
+    }
+}
