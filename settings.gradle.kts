79d35e7e8a3d5f8023e3c8ec36fdf5cf11ba5169
Yash-Garg <yash.garg@outlook.in>
2022-10-11 20:35:19 +0530

    chore: add gradle build scan to publish always

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,16 @@ dependencyResolutionManagement {
     }
 }
 
+plugins { id("com.gradle.enterprise") version "3.11.1" }
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
 enableFeaturePreview("VERSION_CATALOGS")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
