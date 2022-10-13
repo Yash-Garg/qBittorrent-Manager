b50d509088d71bfb9664cbcaf4dc22271d87c92c
Yash-Garg <yash.garg@outlook.in>
2022-10-13 17:50:13 +0530

    fix: use bundle to pass torrent name instead of constructor arg

--- app/src/main/kotlin/dev/yashgarg/qbit/ui/dialogs/RenameTorrentDialog.kt
+++ app/src/main/kotlin/dev/yashgarg/qbit/ui/dialogs/RenameTorrentDialog.kt
@@ -11,7 +11,7 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import dev.yashgarg.qbit.R
 
-class RenameTorrentDialog(private val title: String) : DialogFragment() {
+class RenameTorrentDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreateDialog(savedInstanceState)
         val alertDialogBuilder = MaterialAlertDialogBuilder(requireContext())
@@ -24,6 +24,7 @@ class RenameTorrentDialog(private val title: String) : DialogFragment() {
             setPositiveButton("Rename", null)
         }
 
+        val title = arguments?.getString(TORRENT_NAME_KEY)
         val dialog = alertDialogBuilder.create()
         dialog.window?.setSoftInputMode(5)
 
@@ -31,7 +32,7 @@ class RenameTorrentDialog(private val title: String) : DialogFragment() {
             val nameTil = dialog.findViewById<TextInputLayout>(R.id.torrentName_til)
             val nameTiet = dialog.findViewById<TextInputEditText>(R.id.torrentName_tiet)
             nameTiet?.setText(title)
-            nameTiet?.setSelection(title.length)
+            nameTiet?.setSelection(title?.length ?: 0)
 
             dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
                 if (!nameTiet?.text.isNullOrEmpty()) {
@@ -50,8 +51,9 @@ class RenameTorrentDialog(private val title: String) : DialogFragment() {
     }
 
     companion object {
-        fun newInstance(title: String): RenameTorrentDialog = RenameTorrentDialog(title)
+        fun newInstance(): RenameTorrentDialog = RenameTorrentDialog()
         const val TAG = "RenameTorrentDialogFragment"
+        const val TORRENT_NAME_KEY = "torrent_name"
         const val RENAME_TORRENT_KEY = "rename_torrent"
         const val RENAME_KEY = "rename_fragment"
     }
