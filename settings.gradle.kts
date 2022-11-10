68f83e43788893240f0203170585629b0072ea97
Yash Garg <yash.garg@outlook.in>
2022-11-10 11:44:33 +0530

    chore(deps): bump compose dependencies and don't use SNAPSHOT builds (#168)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
         maven("https://jitpack.io")
     }
 }
