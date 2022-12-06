d8d8bbf5e809565320898b7029cbcebe12b8f490
Yash-Garg <yash.garg@outlook.in>
2022-12-06 23:31:20 +0530

    fix(prefs): directly use application id instead of using BuildConfig

--- app/src/main/kotlin/dev/yashgarg/qbit/data/preferences/ServerPreferencesSerializer.kt
+++ app/src/main/kotlin/dev/yashgarg/qbit/data/preferences/ServerPreferencesSerializer.kt
@@ -2,7 +2,6 @@ package dev.yashgarg.qbit.data.preferences
 
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
-import dev.yashgarg.qbit.BuildConfig
 import dev.yashgarg.qbit.data.models.ServerPreferences
 import java.io.InputStream
 import java.io.OutputStream
@@ -12,7 +11,7 @@ import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 
 object ServerPreferencesSerializer : Serializer<ServerPreferences> {
-    const val SERVER_PREFS_NAME = "${BuildConfig.APPLICATION_ID}_preferences"
+    const val SERVER_PREFS_NAME = "dev.yashgarg.qbit_preferences"
 
     override val defaultValue = ServerPreferences()
 
