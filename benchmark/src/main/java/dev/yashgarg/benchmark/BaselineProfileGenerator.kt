1d9ef1af82baea30d58c6eac38521ccbdf691418
Yash-Garg <yash.garg@outlook.in>
2022-12-08 17:14:16 +0530

    benchmark: add baseline-prof file and tests

---
+++ benchmark/src/main/java/dev/yashgarg/benchmark/BaselineProfileGenerator.kt
@@ -0,0 +1,22 @@
+package dev.yashgarg.benchmark
+
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BaselineProfileGenerator {
+
+    @get:Rule val baselineRule = BaselineProfileRule()
+
+    @Test
+    fun generateBaselineProfile() =
+        baselineRule.collectBaselineProfile(StartupBenchmark.packageName) {
+            pressHome()
+            startActivityAndWait()
+
+            navigateToConfigFragment()
+        }
+}
--- benchmark/src/main/java/dev/yashgarg/benchmark/UiHelper.kt
+++ benchmark/src/main/java/dev/yashgarg/benchmark/UiHelper.kt
@@ -4,8 +4,8 @@ import androidx.benchmark.macro.MacrobenchmarkScope
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.Until
 
-fun MacrobenchmarkScope.clickAddButton() {
-    val button = device.findObject(By.text("Add server")) // Navigate to Config screen
+fun MacrobenchmarkScope.navigateToConfigFragment() {
+    val button = device.findObject(By.text("Add server"))
     button.click()
 
     device.waitForIdle()
