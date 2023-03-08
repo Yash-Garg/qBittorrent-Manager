dc6f92806c13a46cc41a7b8daf07d3299d749433
Yash Garg <yash.garg@outlook.in>
2023-03-09 00:04:13 +0530

    chore: remove unused sonatype repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
