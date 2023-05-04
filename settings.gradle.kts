e6cfd91e33f7d6a174a8b3499139a2467ede8049
Yash-Garg <yash.garg@outlook.in>
2023-05-04 13:36:08 +0530

    chore(deps): upgrade dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
     }
 }
 
-plugins { id("com.gradle.enterprise") version "3.13" }
+plugins { id("com.gradle.enterprise") version "3.13.1" }
 
 gradleEnterprise {
     buildScan {
